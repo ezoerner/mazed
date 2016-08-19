98138012bffa99fc57a4aab63b1a7d909dde9d03
Eric Zoerner <eric.zoerner@gmail.com>
2016-08-19 16:41:27 -0700

    displaying Maze

--- src/main/scala/mazed/maze/Maze.scala
+++ src/main/scala/mazed/maze/Maze.scala
@@ -27,14 +27,14 @@ case class Dir(asInt: Int) extends AnyVal {
   def includes(dir: Dir): Boolean = (this.asInt & dir.asInt) != 0
 }
 
-private case class Cell(asXY: (Int, Int)) extends AnyVal {
+case class Cell(asXY: (Int, Int)) extends AnyVal {
   def x = asXY._1
   def y = asXY._2
   def step(dir: Dir): Cell = dir match {
     case East ⇒ Cell((x + 1, y))
     case West ⇒ Cell((x - 1, y))
-    case North ⇒ Cell((x + 1, y - 1))
-    case South ⇒ Cell((x + 1, y + 1))
+    case North ⇒ Cell((x, y - 1))
+    case South ⇒ Cell((x, y + 1))
     case d ⇒ sys.error("unsupported direction in step: $d")
   }
 }
@@ -48,6 +48,7 @@ object Maze {
       }
 
       def go(active: IndexedSeq[Cell], grid: Maze): Maze = {
+        println(grid.manifestAsString)
         if (active.isEmpty) {
           grid
         }
@@ -57,14 +58,14 @@ object Maze {
 
           val maybeUnknownDir: Option[Dir] = Random.shuffle(List(North, South, East, West)).find { dir ⇒
             val neighbor = cell step dir
-            (grid inBounds neighbor) && grid(neighbor.x, neighbor.y) == Unknown
+            (grid inBounds neighbor) && grid(neighbor) == Unknown
           }
 
           maybeUnknownDir.fold[Maze](go(active removeAt index, grid)) { dir ⇒
             val neighbor = cell step dir
-            val dirHere = grid(cell.x, cell.y)
-            val hereUpdated = grid.updated(cell.x, cell.y, dirHere + dir)
-            val neighborUpdated = hereUpdated.updated(neighbor.x, neighbor.y, -dir)
+            val dirHere = grid(cell)
+            val hereUpdated = grid.updated(cell, dirHere + dir)
+            val neighborUpdated = hereUpdated.updated(neighbor, -dir)
             go(active :+ neighbor, neighborUpdated)
           }
         }
@@ -75,15 +76,62 @@ object Maze {
     val grid = Maze(Vector.fill[Dir](height, width)(Unknown))
     go(active, grid)
   }
+
+  def main(args: Array[String]): Unit = {
+    val maze = generate(args(0).toInt, args(1).toInt)
+    println(maze.manifestAsString)
+  }
 }
 
 /** Each cell in a Grid represents an intersection of any combination
   * of the four directions.
   */
 case class Maze(asVector: Vector[Vector[Dir]]) extends AnyVal {
-  def apply(x: Int, y: Int): Dir = asVector(y)(x)
-  def updated(x: Int, y: Int, dir: Dir): Maze = Maze(asVector.updated(y, asVector(y).updated(x, dir)))
+  def apply(cell: Cell): Dir = try asVector(cell.y)(cell.x) catch {
+    case e: IndexOutOfBoundsException ⇒ throw new RuntimeException(s"${cell.x},${cell.y}", e)
+  }
+  def updated(cell: Cell, dir: Dir): Maze = Maze(asVector.updated(cell.y, asVector(cell.y).updated(cell.x, dir)))
+
   def width = asVector(0).size
+
   def height = asVector.size
-  def inBounds(cell: Cell): Boolean = cell.x >= 0 && cell.y >= 0 && cell.x < width && cell.y < height
+
+  def inBounds(cell: Cell): Boolean = xInBounds(cell.x) && yInBounds(cell.y)
+  def yInBounds(y: Int): Boolean = y >= 0 && y < height
+  def xInBounds(x: Int): Boolean = x >= 0 && x < width
+
+  def manifestAsString: String = {
+    val builder = new StringBuilder()
+    builder.append(" ")
+    builder.append(List.fill(width * 2 - 1)('_').mkString)
+    builder.append("\n")
+    asVector.zipWithIndex foreach {
+      case (row, y) ⇒
+        builder.append("|")
+        row.zipWithIndex foreach {
+          case (dirHere: Dir, x) ⇒
+            val cellHere = Cell((x, y))
+            val cellBelow = cellHere step South
+            val cellRight = cellHere step East
+            val cellBelowRight = cellBelow step East
+            val noFloor1 = (dirHere == Unknown && yInBounds(cellBelow.y) && apply(cellBelow) == Unknown) ||
+                           (dirHere includes South)
+            builder.append(if (noFloor1) " " else "_")
+
+            if (dirHere == Unknown && xInBounds(cellRight.x) && this(cellRight) == Unknown) {
+              val noFloor2 = yInBounds(cellBelow.y) && (this(cellBelow) == Unknown || this(cellBelowRight) == Unknown)
+              builder.append(if (noFloor2) " " else "_")
+            }
+            else if (dirHere includes East) {
+              val noFloor2 = (dirHere + this(cellRight)) includes South
+              builder.append(if (noFloor2) " " else "_")
+            }
+            else {
+              builder.append("|")
+            }
+        }
+        builder.append("\n")
+    }
+    builder.toString
+  }
 }
